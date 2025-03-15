316a7283606b30781705793736aa8a0e4225dd58
Nguyễn Văn Thương <nvthuong1508@gmail.com>
2025-03-15 14:18:23 +0700

    #Add lib for working with azure storage

---
+++ src/nutget-package/TN.Azure.Storage/Models/SASToken.cs
@@ -0,0 +1,9 @@
+﻿namespace TN.Azure.Storage.Models
+{
+    public class SASToken
+    {
+        public string SasToken { get; set; }
+        public string CdnFormatEndpoint { get; set; }
+        public string BlobEndpoint { get; set; }
+    }
+}
---
+++ src/nutget-package/TN.Azure.Storage/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TN.Azure.Storage.Services;
+using TN.Azure.Storage.Services.Abtractions;
+
+namespace TN.Azure.Storage
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddAzureStorageService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<AzureStorageOptions>(options => configuration.GetSection(nameof(AzureStorageOptions)).Bind(options));
+            services.AddSingleton<IAzureStorageService, AzureStorageService>();
+            return services;
+        }
+    }
+}
